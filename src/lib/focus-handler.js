838ae3c2f4b630fcdd7a4dfef86fa1e546183cb7
LekoArts <lekoarts@gmail.com>
2022-08-16 17:26:59 +0200

    change focusHandlerImpl

--- src/lib/focus-handler.js
+++ src/lib/focus-handler.js
@@ -16,28 +16,116 @@ export const FocusHandler = ({ uri, location, component, ...domProps }) => {
   )
 }
 
+const FocusHandlerImpl = ({
+  children,
+  style,
+  requestFocus,
+  component: Comp = "div",
+  uri,
+  location,
+  ...domProps
+}) => {
+  let node = React.useRef()
+
+  const [shouldFocus, setShouldFocus] = React.useState(true)
+  const [prevUri, setPrevUri] = React.useState(uri)
+  const [prevPathname, setPrevPathname] = React.useState(location.pathname)
+  const [focusHandlerCount, setFocusHandlerCount] = React.useState(0)
+  const initialRender = React.useRef(true)
+
+  if (uri) {
+    const myURIChanged = uri !== prevUri
+    const navigatedUpToMe =
+      prevPathname !== location.pathname && location.pathname === uri
+
+    setPrevUri(uri)
+    setPrevPathname(location.pathname)
+
+    setShouldFocus(myURIChanged || navigatedUpToMe)
+  }
+
+  React.useEffect(() => {
+    setFocusHandlerCount(f => f + 1)
+    focus()
+
+    return () => {
+      setFocusHandlerCount(f => f - 1)
+      if (focusHandlerCount === 0) {
+        initialRender.current = true
+      }
+    }
+  }, [])
+
+  React.useEffect(() => {
+    if (shouldFocus) {
+      focus()
+    }
+  }, [location])
+
+  const focus = () => {
+    if (process.env.NODE_ENV === "test") {
+      // TODO: Still a problem?
+      // getting cannot read property focus of null in the tests
+      // and that bit of global `initialRender` state causes problems
+      return
+    }
+
+    if (requestFocus) {
+      requestFocus(node.current)
+    } else {
+      if (initialRender.current) {
+        initialRender.current = false
+      } else if (node) {
+        // React polyfills [autofocus] and it fires earlier than cDM, so we were stealing focus away, this line prevents that.
+        if (!node.current.contains(document.activeElement)) {
+          node.current.focus()
+        }
+      }
+    }
+  }
+
+  const _requestFocus = node => {
+    if (!shouldFocus && node) {
+      node.current.focus()
+    }
+  }
+
+  return (
+    <Comp
+      style={{ outline: "none", ...style }}
+      tabIndex="-1"
+      ref={n => (node = n)}
+      {...domProps}
+    >
+      <FocusContext.Provider value={_requestFocus}>
+        {children}
+      </FocusContext.Provider>
+    </Comp>
+  )
+}
+
 // don't focus on initial render
 let initialRender = true
 let focusHandlerCount = 0
 
-class FocusHandlerImpl extends React.Component {
+class FocusHandlerImpl2 extends React.Component {
   state = {}
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const initial = prevState.uri == null
+  static getDerivedStateFromProps(props, state) {
+    const initial = state.uri == null
     if (initial) {
       return {
         shouldFocus: true,
-        ...nextProps,
+        ...props,
       }
     } else {
-      const myURIChanged = nextProps.uri !== prevState.uri
+      const myURIChanged = props.uri !== state.uri
       const navigatedUpToMe =
-        prevState.location.pathname !== nextProps.location.pathname &&
-        nextProps.location.pathname === nextProps.uri
+        state.location.pathname !== props.location.pathname &&
+        props.location.pathname === props.uri
       return {
         shouldFocus: myURIChanged || navigatedUpToMe,
-        ...nextProps,
+        ...props,
       }
     }
   }
